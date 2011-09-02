6b89a54da98b9ad37a4dd0a0bc31e825b7035205
Kevin Griffin <griffin.kev@gmail.com>
2011-09-02 10:47:46 -0400

    toasts can now be cancelled

--- src/com/chariotsolutions/toast/plugin/ToastPlugin.java
+++ src/com/chariotsolutions/toast/plugin/ToastPlugin.java
@@ -1,54 +1,61 @@
 package com.chariotsolutions.toast.plugin;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-
 import android.util.Log;
 import android.widget.Toast;
-
 import com.phonegap.api.Plugin;
 import com.phonegap.api.PluginResult;
 import com.phonegap.api.PluginResult.Status;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class ToastPlugin extends Plugin {
 
-	private static final String TAG = "ToastPlugin";
-	private static final String LONG_TOAST_ACTION = "show_long";
-	private static final int TOAST_MESSAGE_INDEX = 0;
+    private static final String TAG = "ToastPlugin";
+    private static final String LONG_TOAST_ACTION = "show_long";
+    private static final String CANCEL_ACTION = "cancel";
+    private static final int TOAST_MESSAGE_INDEX = 0;
 
-	@Override
-	public PluginResult execute(String action, JSONArray data, String callbackId) {
-		String toastMessage;
-		try {
-			toastMessage = data.getString(TOAST_MESSAGE_INDEX);
-		} catch (JSONException e) {
-			Log.e(TAG, "Required parameter 'Toast Message' missing");
-			return new PluginResult(Status.ERROR);
-		}
-		
-		if (action.equals(LONG_TOAST_ACTION)) {
-		  ctx.runOnUiThread(new RunnableToast(toastMessage, Toast.LENGTH_LONG));
-		} else {
-		  ctx.runOnUiThread(new RunnableToast(toastMessage, Toast.LENGTH_SHORT));
-		}
+    private Toast toast = null;
+    private String toastMessage;
+    @Override
+    public PluginResult execute(String pluginAction, JSONArray data, String callbackId) {
+        Log.d(TAG, pluginAction);
+        if (pluginAction.equals(CANCEL_ACTION)) {
 
-		return new PluginResult(Status.OK);
-	}
+            this.ctx.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (toast != null) toast.cancel();
+                }
+            });
+        } else {
+            try {
+                toastMessage = data.getString(TOAST_MESSAGE_INDEX);
+            } catch (JSONException e) {
+                Log.e(TAG, "Required parameter 'Toast Message' missing");
+                return new PluginResult(Status.ERROR);
+            }
 
-	class RunnableToast implements Runnable {
-		private String message;
-		private int length;
+            if (pluginAction.equals(LONG_TOAST_ACTION)) {
+                Log.d(TAG, "long?");
+                this.ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        toast = Toast.makeText(ctx, toastMessage, Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                });
+            } else {
+                this.ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        toast = Toast.makeText(ctx, toastMessage, Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+            }
+        }
 
-		public RunnableToast(String message, int length) {
-			this.message = message;
-			this.length = length;
-		}
-
-		@Override
-		public void run() {
-			Toast.makeText(ctx, message, length).show();
-		}
-
-	}
-
+        return new PluginResult(Status.OK);
+    }
 }
