47d6a657b49e91a104eb1a9af968842924deefaf
Don Coleman <dcoleman@chariotsolutions.com>
2013-07-05 16:44:36 -0400

    don't version copied files

--- examples/toastify/platforms/android/assets/www/cordova_plugins.js
+++
@@ -1,3 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [{"file":"plugins/com.chariotsolutions.toast.plugin/www/phonegap-toast.js","id":"com.chariotsolutions.toast.plugin.Toasty","clobbers":["toast"]}]
-});
